6b3b1691177447a024b6f0044181d3e8997daf13
The Future Pirate King <kasianenkomykyta@gmail.com>
2026-05-30 09:40:28 +0200

    ref: ref example

--- examples/main.py
+++ examples/main.py
@@ -1,9 +1,14 @@
+from fastapi import FastAPI
+
 from openadmin import AdminPanel
 
 from .posts import page as posts_admin
 from .users import page as users_admin
 
-panel = AdminPanel()
+app = FastAPI()
+admin_panel = AdminPanel()
 
-panel.include_page(posts_admin, tags=["Posts"])
-panel.include_page(users_admin, tags=["Users"])
+admin_panel.include_page(posts_admin, tags=["Posts"])
+admin_panel.include_page(users_admin, tags=["Users"])
+
+app.mount("/admin", admin_panel)
